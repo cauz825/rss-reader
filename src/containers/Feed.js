3a018b0bf99b1f7f42ca37ed629c9f41dbca4603
MCauz <michaelhcausey@gmail.com>
2020-10-12 17:06:12 -0700

    Article titles populating

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Feed() {
+
+    const [articles, setArticles] = useState([])
+
+    
+    function handleClick() {
+        fetch('https://fivethirtyeight.com/sports/feed/')
+        .then(resp => resp.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => parseData(data))
+    }
+
+    function parseData(data) {
+        let articleArray = Array.from(data.querySelectorAll("item"))
+        setArticles(articleArray)
+    }
+
+    function displayArticles(article) {
+        let title = article.querySelector("title").innerHTML
+        return(<h4>{title}</h4>)
+    }
+    
     return(    
         <div>
-            Feed 1
-            Feed 2
+            <button onClick={handleClick}>Pull Feed (ESPN)</button>
+            <br></br>
+            {articles 
+            ? articles.map(article => displayArticles(article))
+            : null}
+
+            
         </div>     
     )
 }
