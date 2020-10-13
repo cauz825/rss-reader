c433dcf6b25f686356101db1c645bfc35b5dc8ab
MCauz <michaelhcausey@gmail.com>
2020-10-12 17:31:11 -0700

    538 feed entered

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -4,8 +4,11 @@ function Feed() {
 
     const [articles, setArticles] = useState([])
 
-    
     function handleClick() {
+        fetch538()
+    }
+
+    function fetch538() {
         fetch('https://fivethirtyeight.com/sports/feed/')
         .then(resp => resp.text())
         .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
@@ -19,14 +22,22 @@ function Feed() {
 
     function displayArticles(article) {
         let title = article.querySelector("title").innerHTML
-        return(<h4>{title}</h4>)
+        let artUrl = article.querySelector("link").innerHTML
+        let pubDate = article.querySelector('pubDate').innerHTML
+        return(
+            <div>
+                <a href={artUrl}><h4>{title}</h4></a>
+                <p>{pubDate}</p>
+            </div>
+        )
     }
     
     return(    
         <div>
-            <button onClick={handleClick}>Pull Feed (ESPN)</button>
+            <button onClick={handleClick}>Pull Feed (538)</button>
             <br></br>
             {articles 
+            // ? console.log(articles[0])
             ? articles.map(article => displayArticles(article))
             : null}
 
